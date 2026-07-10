ca45e6fb82ddbad07c45b0ea4eacfea0ecafef5c
Amanshrivastav <shrivastavaman2004@gmail.com>
2026-07-10 23:20:17 +0530

    feat: redirect unauthenticated users to signin, remove alert

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Signup from './pages/Signup'
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import { BrowserRouter as Router , Routes , Route , Navigate } from 'react-router-dom'
 import Signin from './pages/Signin'
 import { Verify } from './pages/verify'
 import { ForgotPassword } from './pages/forgotPassword'
 import Home from './pages/Home'
 import Document from './pages/Document'
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const token = sessionStorage.getItem("token")
+  return token ? <>{children}</> : <Navigate to="/Signin" replace />
+}
 
 function App() {
   
@@ -20,8 +25,9 @@ function App() {
           <Route path="/verifyemail/:verificationtoken" element={<Verify/>} />
           <Route path="/forgot-password" element={<ForgotPassword/>} />
           <Route path="/reset-password/:resetToken" element={<ForgotPassword/>} />
-          <Route path="/" element={<Home/>} />
-          <Route path='/document/:docId' element={<Document/>}/>
+          <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
+          <Route path='/document/:docId' element={<ProtectedRoute><Document/></ProtectedRoute>}/>
+          <Route path="*" element={<Navigate to="/Signin" replace />} />
         </Routes>
       </Router> 
     </>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,25 +1,18 @@
 
 import Navbar from "../components/Navbar"
 import { Body } from "../components/Body"
-import { useNavigate } from "react-router-dom";
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 import { useStore } from '../store/zustand';
 
 
 const Home=()=>{
 
- const navigate = useNavigate()
- const token = sessionStorage.getItem("token")
  const getDocuments = useStore((state) => state.getDocuments)
 
- useLayoutEffect(()=>{
-     if(!token){
-         alert("Please login first")
-         navigate('/Signin')
-     } else {
-         getDocuments()
-     }
+ useEffect(()=>{
+     getDocuments()
  }, [])
+
 
 
     return(
